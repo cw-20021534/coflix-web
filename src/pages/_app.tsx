e46f9220ba6513b484f7afbb2653c9e2f6903b8a
cw-20021534 <alsdndjdrktl@coway.com>
2022-09-27 14:38:20 +0900

    feat(#1): 헤더 구현
    - layout 타입 추가
    - layout 구현
    - GNB 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import '../../styles/globals.css';
-import type { AppProps } from 'next/app';
+import '../../styles/globals.scss';
 import { QueryClient, QueryClientProvider, Hydrate } from '@tanstack/react-query';
 import { useState } from 'react';
+import type { AppPropsWithLayout } from '~/types';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const [queryClient] = useState(() => new QueryClient());
+
+  const getLayout = Component.getLayout || ((page) => page);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
+      <Hydrate state={pageProps.dehydratedState}>{getLayout(<Component {...pageProps} />)}</Hydrate>
     </QueryClientProvider>
   );
 }
